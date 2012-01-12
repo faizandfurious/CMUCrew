832fa67cb81af4f591cca369c032cf00a0d273fb
Faiz Abbasi <fabbasi@andrew.cmu.edu>
2012-01-12 11:04:25 -0800

    First commit. I also changed the README, check it out

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CMUCrew::Application.config.secret_token = 'a147596496732511934b2bd4c085aac17a2743d57bda60a3acf1a0d62632f8a7717c31e6cdb45cb70a5a02dcec422441f38706a0e7d949a8f94b8d225235c021'
