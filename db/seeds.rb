99723985397601325a13e80b8a910949501a7566
Faiz <fabbasi@andrew.cmu.edu>
2012-07-19 17:53:26 -0400

    Created normal user in seeds. Added placeholder sign up button for fundraisers page

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,23 +9,26 @@
 
 #Myself (Admin rights)
 admin = Role.find_or_create_by_name({name: 'Admin'})
-faiz = User.find_or_create_by_email(email: 'fabbasi@andrew.cmu.edu', first_name: 'Faiz', last_name: 'Abbasi' password: 'admin1', password_confirmation: 'admin1')
+faiz = User.find_or_create_by_email(email: 'fabbasi@andrew.cmu.edu', first_name: 'Faiz', last_name: 'Abbasi', password: 'admin1', password_confirmation: 'admin1')
 faiz_admin = UserRole.find_or_create_by_user_id(user_id:faiz.id, role_id:admin.id)
 
 #Fundraisers
 fundraiser = Role.find_or_create_by_name({name: 'Fundraiser'})
-kelsey = User.find_or_create_by_email(email: 'kdiffley@andrew.cmu.edu', first_name: 'Kelsey', last_name: 'Diffley' password: 'password', password_confirmation: 'password')
-jinie = User.find_or_create_by_email(email: 'jinie@andrew.cmu.edu', first_name: 'Jinie', last_name: 'Haytko' password: 'password', password_confirmation: 'password')
+kelsey = User.find_or_create_by_email(email: 'kdiffley@andrew.cmu.edu', first_name: 'Kelsey', last_name: 'Diffley', password: 'password', password_confirmation: 'password')
+jinie = User.find_or_create_by_email(email: 'jinie@andrew.cmu.edu', first_name: 'Jinie', last_name: 'Haytko', password: 'password', password_confirmation: 'password')
 kelsey_fundraiser = UserRole.find_or_create_by_user_id(user_id: kelsey.id, role_id: fundraiser.id)
 jinie_fundraiser = UserRole.find_or_create_by_user_id(user_id: jinie.id, role_id: fundraiser.id)
 
 #Coach
-coach = Role.find_or_create_by_user_id({name: 'Coach'})
-alyssa = User.find_or_create_by_name(email:'alyssa@andrew.cmu.edu', first_name: 'Alyssa', last_name: 'Fogel', password: 'password', password_confirmation: 'password')
+coach = Role.find_or_create_by_name({name: 'Coach'})
+alyssa = User.find_or_create_by_email(email:'alyssa@andrew.cmu.edu', first_name: 'Alyssa', last_name: 'Fogel', password: 'password', password_confirmation: 'password')
 alyssa_coach = UserRole.find_or_create_by_user_id(user_id: alyssa.id, role_id: coach.id)
+
+#Normal User
+wesley = User.find_or_create_by_email(email:'wesley@andrew.cmu.edu', first_name: 'Wesley', last_name: 'Transue', password: 'password', password_confirmation: 'password')
+
 
 #Fundraiser Types
 steelers = FundraiserType.find_or_create_by_name(name: 'Steelers Game')
 rentarower = FundraiserType.find_or_create_by_name(name: 'Rent-A-Rower')
 gwensgirls = FundraiserType.find_or_create_by_name(name: 'Gwens Girls')
-
