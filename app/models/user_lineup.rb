0a511c5523fa34c58b30b10fa1b0405f54290134
Faiz <fabbasi@andrew.cmu.edu>
2012-08-03 23:50:21 -0400

    Elaborated relationships within user_lineups

--- app/models/user_lineup.rb
+++ app/models/user_lineup.rb
@@ -2,7 +2,8 @@ class UserLineup < ActiveRecord::Base
 	attr_accessible :user_id, :lineup_id, :seat
 
 	#Relationships
-	belongs_to :user, :lineup
+	belongs_to :user
+	belongs_to :lineup
 
 	#Constants
 	SEATS = {'0' => 'Coxswain', '1' => 'One Seat', '2' => 'Two Seat',  '3' => 'Three Seat', '4' => 'Four Seat', '5' => 'Five Seat', '6' => 'Six Seat', '7' => 'Seven Seat', '8' => 'Eight Seat' }
