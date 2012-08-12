63a88a630811875a4e15e90deeec1a6a5237f617
Faiz <fabbasi@andrew.cmu.edu>
2012-08-12 15:09:30 -0700

    Changes to style of the site.

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'rails', '3.2.2'
 gem 'pg'
 gem 'twitter-bootstrap-rails'
+gem 'bootstrap-datepicker-rails'
 
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
--- app/models/practice.rb
+++ app/models/practice.rb
@@ -1,2 +1,13 @@
 class Practice < ActiveRecord::Base
+
+#Relationships
+has_many :lineups, :through => :practice_lineups
+belongs_to :workout_type
+
+#Scopes
+
+#Constants
+LOCATIONS = {'1' => "TRRA Boathouse", '2' => "Skibo Gymnasium"}
+
+
 end
