95a3b29655226c6a4200803c7838629d8fa76a1b
Faiz <fabbasi@andrew.cmu.edu>
2012-08-18 21:21:12 +0200

    jQuery interaction with lineups is now promising

--- app/assets/javascripts/lineups.js
+++ app/assets/javascripts/lineups.js
@@ -1,19 +1,22 @@
 $(document).ready(function () {    
        $("#lineup_boat_id").bind("change", function () {
-        if ($(this).val() == "1") {
-            $(".example").slideDown();
-            $("#pro2").slideUp();
+        if ($(this).val() == "1x") {
+            $(".example").text("You have selected a " + $(this).val());
         }
-        else if($(this).val() =="2") {
-            $("").slideDown();
-            $(".example").slideUp();
+        else if($(this).val() =="2-") {
+            $(".example").text("You have selected a " + $(this).val());
+        }
+        else if($(this).val() =="2x") {
+            $(".example").text("You have selected a " + $(this).val());
+        }
+        else if($(this).val() =="4+") {
+            $(".example").text("You have selected a " + $(this).val());
+        }
+        else if($(this).val() =="4x") {
+            $(".example").text("You have selected a " + $(this).val());
+        }
+        else if($(this).val() =="8+") {
+            $(".example").text("You have selected a " + $(this).val());
         }
     });
 });
-
-
-function getName(){
-    alert $("#lineup_boat_id").data('name')
-}
-
-alert $('#products').data('url')
