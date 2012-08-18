fd0056b80192dff7642643b978f09bd2117ce8bf
Faiz <fabbasi@andrew.cmu.edu>
2012-08-18 18:51:50 +0200

    Removed lineups.js code for heroku

---
+++ app/assets/javascripts/lineups.js
@@ -0,0 +1,19 @@
+// $(document).ready(function () {    
+//        $("#lineup_boat_id").bind("change", function () {
+//         if ($(this).val() == "1") {
+//             $(".example").slideDown();
+//             $("#pro2").slideUp();
+//         }
+//         else if($(this).val() =="2") {
+//             $("").slideDown();
+//             $(".example").slideUp();
+//         }
+//     });
+// });
+
+
+// function getName(){
+//     alert $("#lineup_boat_id").data('name')
+// }
+
+// alert $('#products').data('url')
